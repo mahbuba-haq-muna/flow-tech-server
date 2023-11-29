dce4cb1037220acfbc6a80fee4e054c22022bc6e
muna <muna3828@gmail.com>
2023-11-29 21:45:10 +0600

    get and post data in reviews

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygezfuj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,12 +30,21 @@ async function run() {
     const featuredCollection = client.db("TechDb").collection("featured");
     const trendingCollection = client.db("TechDb").collection("trending");
     const productCollection = client.db("TechDb").collection("products");
+    const reviewCollection = client.db("TechDb").collection("reviews");
+    const userCollection = client.db("TechDb").collection("users");
 
     // featured products related api
     app.get('/featured', async(req, res) =>{
         const result = await featuredCollection.find().toArray()
         res.send(result)
     });
+
+    app.get('/featured/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await featuredCollection.findOne(query);
+        res.send(result);
+      })
 
     // trending products related api
     app.get('/trending', async(req, res) =>{
@@ -45,9 +54,46 @@ async function run() {
 
     // all products related api
     app.get('/products', async(req, res) =>{
-        const result = await productCollection.find().toArray()
+        // const filter = req.query.search
+        // console.log(filter)
+        // const query = {
+        //     tags:{ $regex: filter, $options: 'i'}
+        // }
+        const cursor = productCollection.find()
+        const result = await cursor.toArray()
         res.send(result)
     });
+
+    
+
+    app.get('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await productCollection.findOne(query);
+        res.send(result);
+      })
+
+    //   review
+
+    app.post('/reviews', async (req, res) =>{
+        const reviews = req.body;
+        console.log(reviews);
+        const result = await reviewCollection.insertOne(reviews);
+        res.send(result)
+    })
+
+    app.get('/reviews', async(req, res) =>{
+        const result = await reviewCollection.find().toArray()
+        res.send(result)
+    });
+
+    // user
+    app.post('/users', async (req, res) =>{
+        const users = req.body;
+        console.log(users);
+        const result = await userCollection.insertOne(users);
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
